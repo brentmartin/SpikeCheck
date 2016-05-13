6a0f6d0aaf31f58580f685ac833dea1b8972d0fd
Brent Martin <bmart35@gmail.com>
2016-05-12 21:39:27 -0500

    included strong params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:body, :context, :assessment_id)
+    end
 end
